37c80a9201110265888808b087dc6b77b8db135c
Cezary Kłos <cezary.klos@gmail.com>
2022-08-03 20:05:09 +0200

    working sudoku

--- sudoku/cell.rb
+++ sudoku/cell.rb
@@ -1,9 +1,12 @@
+# definition of a Cell
+# Cell represents single field of 9x9 sudoku grid
+# candidates are possible numbers that do not exist in cell's row, column or square
 class Cell
   attr_accessor :candidates, :solved, :number
 
   def initialize(solved: false, number: nil)
+    @solved = solved
     if solved
-      @solved = solved
       @number = number
     else
       @candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9]
--- sudoku/sudoku.rb
+++ sudoku/sudoku.rb
@@ -35,15 +35,20 @@ class Sudoku
     @columns = Array.new(9) { [] }
     @squres = Array.new(9) { [] }
 
-    fill_cells
-    (0..80).each do |cell|
-      @rows[row_number(cell)] << cell
-      @columns[column_number(cell)] << cell
-    end
+    load_input_into_cells
+    assign_cells_to_rows_columns_sqares
 
     return ['can\'t be solved'] unless soduku_valid?
 
-    # puts valid?(@columns[0])
+    solved = false
+    until solved
+      @rows.each { |row| solve_part(row) }
+      @columns.each { |column| solve_part(column) }
+      @squres.each { |square| solve_part(square) }
+      solved = solved?
+    end
+    @input = cells_to_string
+    to_s
   end
 
   private
@@ -62,44 +67,19 @@ class Sudoku
     row.insert(3, '|')
   end
 
-  def valid_block?(values)
-    values.delete(0)
-    values.uniq.length == values.length
-  end
-
-  def row(row_no)
-    @input.slice((row_no * 9), 9)
-  end
-
   def row_number(cell_id)
     cell_id / 9
-  end
-
-  def column(col_no)
-    column = ''
-    (0..8).each do |i|
-      column << @input[i * 9 + col_no]
-    end
-    column
   end
 
   def column_number(cell_id)
     cell_id % 9
   end
 
-  # def square(square_no)
-  #   square = ''
-  #   (0.8).each do |i|
-
-  #   end
-  #   square
-  # end
-
   def square_number(cell_id)
     row_number(cell_id) / 3 * 3 + column_number(cell_id) / 3
   end
 
-  def fill_cells
+  def load_input_into_cells
     @input.each_char do |char|
       cell = if char == '0'
                Cell.new
@@ -110,30 +90,67 @@ class Sudoku
     end
   end
 
+  def assign_cells_to_rows_columns_sqares
+    (0..80).each do |index|
+      @rows[row_number(index)] << @cells[index]
+      @columns[column_number(index)] << @cells[index]
+      @squres[square_number(index)] << @cells[index]
+    end
+  end
+
   def soduku_valid?
     result = true
     @rows.each do |row|
       result = valid?(row)
       return false unless result
     end
-    @columns.each do |row|
-      result = valid?(row)
+    @columns.each do |column|
+      result = valid?(column)
+      return false unless result
+    end
+    @squres.each do |square|
+      result = valid?(square)
       return false unless result
     end
 
     result
   end
 
-  def valid?(chunk)
+  def valid?(part)
     values = Set[]
-    chunk.each do |index|
-      puts "cell id: #{index}, solved: #{@cells[index].solved}"
-      next unless @cells[index].solved
+    part.each do |cell|
+      next unless cell.solved
 
-      puts values.inspect
-
-      return false if values.add?(@cells[index].number).nil?
+      return false if values.add?(cell.number).nil?
     end
     true
+  end
+
+  def solve_part(part)
+    values = []
+    part.each do |cell|
+      values << cell.number if cell.solved
+    end
+    part.each do |cell|
+      next if cell.solved
+
+      cell.candidates = cell.candidates.difference(values)
+      if cell.candidates.one?
+        cell.solved = true
+        cell.number = cell.candidates.pop
+      end
+    end
+  end
+
+  def solved?
+    @cells.none? { |cell| cell.solved == false }
+  end
+
+  def cells_to_string
+    output = ''
+    @cells.each do |cell|
+      output << cell.number.to_s
+    end
+    output
   end
 end
