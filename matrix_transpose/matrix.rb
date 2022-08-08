377b2ccfc999fa03cba60ee49315759f335201a8
Cezary Kłos <cezary.klos@gmail.com>
2022-08-08 16:27:32 +0200

    matrix_tranpose solution

--- matrix_transpose/matrix.rb
+++ matrix_transpose/matrix.rb
@@ -1,3 +1,18 @@
 class Matrix
-  # Add your code here
+  def initialize(input)
+    @input = input
+  end
+
+  def transpose
+    matrix = @input.split("\n").map { |row| row.delete(' ') }
+    transposed_matrix = Array.new(matrix[0].length) { [] }
+
+    matrix.each_with_index do |row, row_index|
+      row.each_char.with_index do |char, char_index|
+        transposed_matrix[char_index][row_index] = char
+      end
+    end
+
+    transposed_matrix.map { |row| row.join(' ') }.join("\n")
+  end
 end
