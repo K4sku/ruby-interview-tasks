0704be4415e4a045ded5a7f326b9ec25d52a8269
Cezary Kłos <cezary.klos@gmail.com>
2022-08-17 19:32:44 +0200

    fibbonaci with loop

--- fibbonacci_generator/fibonacci_numbers_generator.rb
+++ fibbonacci_generator/fibonacci_numbers_generator.rb
@@ -1,3 +1,19 @@
 class FibonacciNumbersGenerator
+  def self.find_nth(nth)
+    last_number = 0
+    next_number = 0
+    current_number = 1
+    (nth - 1).times do
+      next_number = count_next(last_number, current_number)
+      last_number = current_number
+      current_number = next_number
+    end
+    current_number
+  end
 
+  class << self
+    def count_next(last, current)
+      last + current
+    end
+  end
 end
